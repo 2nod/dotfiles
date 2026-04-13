fc7bd810d0d32dffe1a89c8e696d0befc776513a
tsuno <42732270+tsuno@users.noreply.github.com>
2026-04-13 19:18:14 +0900

    nvim: drop nvim-treesitter plugin

--- nvim/lua/config/base.lua
+++ nvim/lua/config/base.lua
@@ -1,7 +1,2 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-local treesitter_grammars = vim.env.TREESITTER_GRAMMARS
-if treesitter_grammars and treesitter_grammars ~= "" then
-	vim.opt.runtimepath:append(treesitter_grammars)
-end
